79f331a761346b12699d4cc2f8c903bffa000148
DirectDev <69155843+Directinho@users.noreply.github.com>
2024-06-19 10:08:41 -0300

    Update server.js

--- server.js
+++ server.js
@@ -9,33 +9,48 @@ const filmesPath = path.join(__dirname, 'filmes.json');
 const filmesData = fs.readFileSync(filmesPath, 'utf-8');
 const filmes = JSON.parse(filmesData);
 
-function criarCard(filme) {
-    return `
-    <div class="card mb-3" style="max-width: 540px;">
-  <div class="row g-0">
-    <div class="col-md-4">
-      <img src="${filme.cartaz}" class="img-fluid rounded-start" alt="${filme.titulo}">
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title">${filme.titulo}</h5>
-        <p class="card-text">${filme.diretor}</p>
-        <p class="card-text>${filme.genero}</p>
-        <p class="card-text"><small class="text-body-secondary">${filme.ano}</small></p>
-      </div>
-    </div>
-  </div>
-</div>
-`;
+function buscarFilmesPorGenero(genero) {
+    if (!genero) return filmes;
+    return filmes.filter(filme => filme.gênero.toLowerCase() === genero.toLowerCase());
 }
 
-app.get('/', (req, res) => {
-    const CardsHtml = filmes.map(filme => criarCard(filme)).join('');
-    const pageHtmlPath = path.join(__dirname, 'index.html');
-    let pageHtml = fs.readFileSync(pageHtmlPath, 'utf-8');
-    pageHtml = pageHtml.replace('{{CardsHtml}}', CardsHtml);
-    res.send(pageHtml);
+function gerarTabelaDeFilmes(filmes) {
+    let filmesTable = '';
+    filmes.forEach(filme => {
+        filmesTable += `
+        <tr>
+            <td>${filme.título}</td>
+            <td>${filme.gênero}</td>
+            <td>${filme.diretor}</td>
+            <td>${filme.ano}</td>
+            <td><img src="${filme.cartaz}" alt="${filme.título}" style="max-width: 100px;"></td>
+        </tr>
+        `;
+    });
+    return filmesTable;
+}
+
+app.get('/buscar-filme', (req, res) => {
+    const generoBuscado = req.query.genero;
+    const filmesEncontrados = buscarFilmesPorGenero(generoBuscado);
+    const filmesTable = gerarTabelaDeFilmes(filmesEncontrados);
+    const htmlContent = fs.readFileSync(path.join(__dirname, 'dadosfilme.html'), 'utf-8');
+    const finalHtml = htmlContent.replace('{{filmesTable}}', filmesTable);
+
+    res.send(finalHtml);
 });
+
+app.get('/', (req, res) => {
+    const generoBuscado = req.query.genero;
+    const filmesEncontrados = buscarFilmesPorGenero(generoBuscado);
+    const filmesTable = gerarTabelaDeFilmes(filmesEncontrados);
+    const htmlContent = fs.readFileSync(path.join(__dirname, 'dadosfilme.html'), 'utf-8');
+    const finalHtml = htmlContent.replace('{{filmesTable}}', filmesTable);
+
+    res.send(finalHtml);
+});
+
+app.use(express.static(path.join(__dirname)));
 
 app.listen(port, () => {
     console.log(`Servidor iniciado em http://localhost:${port}`);
