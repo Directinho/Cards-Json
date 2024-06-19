eb35c95755a36d1ba0568647515b3c0b58cdac44
DirectDev <69155843+Directinho@users.noreply.github.com>
2024-06-19 12:06:56 -0300

    Update server.js

--- server.js
+++ server.js
@@ -11,46 +11,70 @@ const filmes = JSON.parse(filmesData);
 
 function buscarFilmesPorGenero(genero) {
     if (!genero) return filmes;
-    return filmes.filter(filme => filme.gênero.toLowerCase() === genero.toLowerCase());
-}
-
-function gerarTabelaDeFilmes(filmes) {
-    let filmesTable = '';
-    filmes.forEach(filme => {
-        filmesTable += `
-        <tr>
-            <td>${filme.título}</td>
-            <td>${filme.gênero}</td>
-            <td>${filme.diretor}</td>
-            <td>${filme.ano}</td>
-            <td><img src="${filme.cartaz}" alt="${filme.título}" style="max-width: 100px;"></td>
-        </tr>
-        `;
-    });
-    return filmesTable;
+    return filmes.filter(filme => filme.genero.toLowerCase() === genero.toLowerCase());
 }
 
 app.get('/buscar-filme', (req, res) => {
+    let filmesCards = '';
     const generoBuscado = req.query.genero;
     const filmesEncontrados = buscarFilmesPorGenero(generoBuscado);
-    const filmesTable = gerarTabelaDeFilmes(filmesEncontrados);
-    const htmlContent = fs.readFileSync(path.join(__dirname, 'dadosfilme.html'), 'utf-8');
-    const finalHtml = htmlContent.replace('{{filmesTable}}', filmesTable);
+
+    
+    filmesEncontrados.forEach(filme => {
+ filmesCards += `
+ <div class="card mb-3" style="max-width: 540px;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="${filme.cartaz}" class="img-fluid rounded-start" alt="${filme.titulo}">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">${filme.titulo}</h5>
+                        <p class="card-text">${filme.genero}</p>
+                        <p class="card-text">${filme.diretor}</p>
+                        <p class="card-text"><small class="text-body-secondary">${filme.ano}</small></p>
+                    </div>
+                </div>
+            </div>
+        </div>`;
+    });
+
+    const htmlContent = fs.readFileSync('index.html', 'utf-8');
+    const finalHtml  = htmlContent.replace('{{CardsHtml}}', filmesCards);
 
     res.send(finalHtml);
 });
 
 app.get('/', (req, res) => {
+    let filmesCards = '';
     const generoBuscado = req.query.genero;
     const filmesEncontrados = buscarFilmesPorGenero(generoBuscado);
-    const filmesTable = gerarTabelaDeFilmes(filmesEncontrados);
-    const htmlContent = fs.readFileSync(path.join(__dirname, 'dadosfilme.html'), 'utf-8');
-    const finalHtml = htmlContent.replace('{{filmesTable}}', filmesTable);
+
+    
+    filmesEncontrados.forEach(filme => {
+        filmesCards += `
+        <div class="card mb-3" style="max-width: 540px;">
+        <div class="row g-0">
+            <div class="col-md-4">
+                <img src="${filme.cartaz}" class="img-fluid rounded-start" alt="${filme.titulo}">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title">${filme.titulo}</h5>
+                    <p class="card-text">${filme.genero}</p>
+                    <p class="card-text">${filme.diretor}</p>
+                    <p class="card-text"><small class="text-body-secondary">${filme.ano}</small></p>
+                </div>
+            </div>
+        </div>
+    </div>`;
+    });
+
+    const htmlContent = fs.readFileSync('index.html', 'utf-8');
+    const finalHtml  = htmlContent.replace('{{CardsHtml}}', filmesCards);
 
     res.send(finalHtml);
 });
-
-app.use(express.static(path.join(__dirname)));
 
 app.listen(port, () => {
     console.log(`Servidor iniciado em http://localhost:${port}`);
